74c433b468fc2c856d7f3968bd68bd4726f7dc2f
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-05-16 23:07:21 +0200

    Use ApiVersionKind in CRDs instead of GroupVersionKind to be more consistent with Kubernetes YAML files

--- go/apis/fogapps/v1/servicelevelobjective.go
+++ go/apis/fogapps/v1/servicelevelobjective.go
@@ -1,15 +1,11 @@
 package v1
 
-import (
-	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 // ServiceLevelObjective an SLOs that is attached to
 // a ServiceGraph, a ServiceGraphNode, or a ServiceLink.
 type ServiceLevelObjective struct {
 
 	// Describes the type of SLO.
-	SloType meta.GroupVersionKind `json:"sloType"`
+	SloType ApiVersionKind `json:"sloType"`
 
 	// The name of this SLO instance.
 	//
@@ -17,7 +13,7 @@ type ServiceLevelObjective struct {
 	Name string `json:"name"`
 
 	// The elasticity strategy that should be triggered upon violations of the SLO.
-	ElasticityStrategy meta.GroupVersionKind `json:"elasticityStrategy"`
+	ElasticityStrategy ApiVersionKind `json:"elasticityStrategy"`
 
 	// The SLO-specific configuration.
 	Config *ArbitraryObject `json:"config,omitempty"`
