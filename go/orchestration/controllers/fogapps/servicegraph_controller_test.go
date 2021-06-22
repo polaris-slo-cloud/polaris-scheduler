f7c6cb2e51c2508f6670458629b6fd4d35b8443b
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-06-22 17:31:01 +0200

    Fix nil check

--- go/orchestration/controllers/fogapps/servicegraph_controller_test.go
+++ go/orchestration/controllers/fogapps/servicegraph_controller_test.go
@@ -17,7 +17,7 @@ var _ = Describe("ServiceGraphController", func() {
 
 		It("Creates the deployment resources", func() {
 			graph := createServiceGraph(fogappsCRDs.SimpleReplicaSet)
-			Expect(graph).To(Equal(nil))
+			Expect(graph).To(BeNil())
 		})
 	})
 
