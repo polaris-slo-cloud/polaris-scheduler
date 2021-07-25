eeb4930f84d7af69a93438d83006187cc88909e8
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-07-25 20:24:04 +0200

    Implement RainbowStorageNativeQueryBuilder

--- ts/libs/polaris-streamsight/src/lib/rainbow-storage/internal/rainbow-storage-native-query-builder.ts
+++ ts/libs/polaris-streamsight/src/lib/rainbow-storage/internal/rainbow-storage-native-query-builder.ts
@@ -1,6 +1,18 @@
-import { NativeQueryBuilderBase, TimeSeriesQuery, TimeSeriesQueryResultType } from '@polaris-sloc/core';
+import { NativeQueryBuilderBase, QueryError, TimeSeriesQuery, TimeSeriesQueryResultType } from '@polaris-sloc/core';
 import { PolarisStreamSightConfig } from '../../config';
+import { GetAnalyticsRequest } from '../../model';
+import { RainbowStorageNativeQuery } from './rainbow-storage-native-query';
 
+interface RainbowStorageQueryInfo {
+    query: GetAnalyticsRequest;
+    metricName: string;
+}
+
+/**
+ * Builds queries for the RAINBOW Distributed Data Storage.
+ *
+ * Important: This can currently only handle select statements.
+ */
 export class RainbowStorageNativeQueryBuilder extends NativeQueryBuilderBase  {
 
     constructor(private config: PolarisStreamSightConfig) {
@@ -8,7 +20,22 @@ export class RainbowStorageNativeQueryBuilder extends NativeQueryBuilderBase  {
     }
 
     buildQuery(resultType: TimeSeriesQueryResultType): TimeSeriesQuery<any> {
-        throw new Error('Method not implemented.');
+        const query = this.buildRainbowStorageQuery();
+        return new RainbowStorageNativeQuery(this.config, resultType, query.metricName, query.query);
+    }
+
+    private buildRainbowStorageQuery(): RainbowStorageQueryInfo {
+        if (this.queryChainAfterSelect.length > 0) {
+            throw new QueryError('RainbowStorageNativeQueryBuilder can currently only handle select statements.', this);
+        }
+
+        const metricName = `${this.selectSegment.appName}_${this.selectSegment.metricName}`;
+        return {
+            metricName,
+            query: {
+                key: [ metricName ],
+            },
+        };
     }
 
 }
