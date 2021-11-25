6b517744ba35eeff9d8a148117ce7b1b872068dc
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-11-25 11:25:58 +0100

    Add GetAnnotation and SetAnnotation and refactor object meta utils to work with metav1.Objects

--- go/orchestration/pkg/kubeutil/object_meta_utils.go
+++ go/orchestration/pkg/kubeutil/object_meta_utils.go
@@ -16,18 +16,37 @@ const (
 )
 
 // GetLabel returns the label with the specified key.
-func GetLabel(obj *metav1.ObjectMeta, key string) (string, bool) {
-	if obj.Labels != nil {
-		value, exists := obj.Labels[key]
+func GetLabel(obj metav1.Object, key string) (string, bool) {
+	if labels := obj.GetLabels(); labels != nil {
+		value, exists := labels[key]
 		return value, exists
 	}
 	return "", false
 }
 
 // GetNamespace returns the namespace of the object, or "default" if the namespace is not set.
-func GetNamespace(obj *metav1.ObjectMeta) string {
-	if obj.Namespace != "" {
-		return obj.Namespace
+func GetNamespace(obj metav1.Object) string {
+	if namespace := obj.GetNamespace(); namespace != "" {
+		return namespace
 	}
 	return "default"
+}
+
+// GetAnnotation returns the annotation with the specified key.
+func GetAnnotation(obj metav1.Object, key string) (string, bool) {
+	if annotations := obj.GetAnnotations(); annotations != nil {
+		value, exists := annotations[key]
+		return value, exists
+	}
+	return "", false
+}
+
+// SetAnnotation sets the annotation indicated by the key to the specified value.
+func SetAnnotation(obj metav1.Object, key, value string) {
+	annotations := obj.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+		obj.SetAnnotations(annotations)
+	}
+	annotations[key] = value
 }
--- go/orchestration/pkg/kubeutil/object_meta_utils_test.go
+++ go/orchestration/pkg/kubeutil/object_meta_utils_test.go
@@ -5,20 +5,27 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.rainbow-h2020.eu/rainbow/orchestration/pkg/kubeutil"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("object_meta_utils", func() {
 
-	var metaObj *metav1.ObjectMeta
+	var resourceObj *core.Pod
 
 	BeforeEach(func() {
-		metaObj = &metav1.ObjectMeta{
-			Name:      "TestName",
-			Namespace: "TestNamespace",
-			Labels: map[string]string{
-				"LabelA": "ValueA",
-				"LabelB": "ValueB",
+		resourceObj = &core.Pod{
+			ObjectMeta: meta.ObjectMeta{
+				Name:      "TestName",
+				Namespace: "TestNamespace",
+				Labels: map[string]string{
+					"LabelA": "ValueA",
+					"LabelB": "ValueB",
+				},
+				Annotations: map[string]string{
+					"AnnotationA": "ValueA",
+					"AnnotationB": "ValueB",
+				},
 			},
 		}
 	})
@@ -27,21 +34,21 @@ var _ = Describe("object_meta_utils", func() {
 
 		Context("Existing Labels field", func() {
 			It("returns an existing label", func() {
-				value, found := kubeutil.GetLabel(metaObj, "LabelA")
+				value, found := kubeutil.GetLabel(resourceObj, "LabelA")
 				Expect(found).To(Equal(true))
 				Expect(value).To(Equal("ValueA"))
 			})
 
 			It("reports false on a non-existing label", func() {
-				_, found := kubeutil.GetLabel(metaObj, "DoesNotExist")
+				_, found := kubeutil.GetLabel(resourceObj, "DoesNotExist")
 				Expect(found).To(Equal(false))
 			})
 		})
 
 		Context("Missing Labels field", func() {
 			It("reports false", func() {
-				metaObj.Labels = nil
-				_, found := kubeutil.GetLabel(metaObj, "LabelA")
+				resourceObj.ObjectMeta.Labels = nil
+				_, found := kubeutil.GetLabel(resourceObj, "LabelA")
 				Expect(found).To(Equal(false))
 			})
 		})
@@ -51,12 +58,62 @@ var _ = Describe("object_meta_utils", func() {
 	Describe("GetNamespace", func() {
 
 		It("Returns the set Namespace", func() {
-			Expect(kubeutil.GetNamespace(metaObj)).To(Equal("TestNamespace"))
+			Expect(kubeutil.GetNamespace(resourceObj)).To(Equal("TestNamespace"))
 		})
 
 		It("Returns the default namespace if none is set", func() {
-			metaObj.Namespace = ""
-			Expect(kubeutil.GetNamespace(metaObj)).To(Equal("default"))
+			resourceObj.ObjectMeta.Namespace = ""
+			Expect(kubeutil.GetNamespace(resourceObj)).To(Equal("default"))
+		})
+
+	})
+
+	Describe("GetAnnotation", func() {
+
+		Context("Existing Annotations field", func() {
+			It("returns an existing annotation", func() {
+				value, found := kubeutil.GetAnnotation(resourceObj, "AnnotationA")
+				Expect(found).To(Equal(true))
+				Expect(value).To(Equal("ValueA"))
+			})
+
+			It("reports false on a non-existing annotation", func() {
+				_, found := kubeutil.GetAnnotation(resourceObj, "DoesNotExist")
+				Expect(found).To(Equal(false))
+			})
+		})
+
+		Context("Missing Annotations field", func() {
+			It("reports false", func() {
+				resourceObj.ObjectMeta.Annotations = nil
+				_, found := kubeutil.GetAnnotation(resourceObj, "LabelA")
+				Expect(found).To(Equal(false))
+			})
+		})
+
+	})
+
+	Describe("SetAnnotation", func() {
+
+		Context("Existing Annotations field", func() {
+			It("updates an existing annotation", func() {
+				kubeutil.SetAnnotation(resourceObj, "AnnotationA", "newValue")
+				Expect(resourceObj.ObjectMeta.Annotations["AnnotationA"]).To(Equal("newValue"))
+			})
+
+			It("creates a new annotation", func() {
+				kubeutil.SetAnnotation(resourceObj, "NewAnnotation", "newValue")
+				Expect(resourceObj.ObjectMeta.Annotations["NewAnnotation"]).To(Equal("newValue"))
+			})
+		})
+
+		Context("Missing Annotations field", func() {
+			It("creates an Annotations field", func() {
+				resourceObj.ObjectMeta.Annotations = nil
+				kubeutil.SetAnnotation(resourceObj, "NewAnnotation", "newValue")
+				Expect(resourceObj.ObjectMeta.Annotations).NotTo(Equal(nil))
+				Expect(resourceObj.ObjectMeta.Annotations["NewAnnotation"]).To(Equal("newValue"))
+			})
 		})
 
 	})
