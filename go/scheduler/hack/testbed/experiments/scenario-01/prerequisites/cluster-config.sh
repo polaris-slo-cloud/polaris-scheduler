09ccbf3be56bc149ec677c490b3e41abf921789c
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-01-22 16:22:20 +0100

    Fix custom resource definitions

--- go/scheduler/hack/testbed/experiments/scenario-01/prerequisites/cluster-config.sh
+++ go/scheduler/hack/testbed/experiments/scenario-01/prerequisites/cluster-config.sh
@@ -16,35 +16,35 @@ NODE_COST_LABEL_NAME="rainbow-h2020.eu~1node-cost-per-hour"
 # Fake CPUs in millicores
 declare -A fakeCpus=(
     # Fog nodes
-    ["kind-control-plane"]="4000"
-    ["kind-worker"]="4000"
-    ["kind-worker2"]="4000"
-    ["kind-worker3"]="4000"
-    ["kind-worker4"]="4000"
-    ["kind-worker5"]="4000"
-    ["kind-worker6"]="4000"
+    ["kind-control-plane"]="4000m"
+    ["kind-worker"]="4000m"
+    ["kind-worker2"]="4000m"
+    ["kind-worker3"]="4000m"
+    ["kind-worker4"]="4000m"
+    ["kind-worker5"]="4000m"
+    ["kind-worker6"]="4000m"
 
     # Cloud nodes (represent node types, the configured resources are actualAvailableResource * 1000)
-    # ["kind-worker7"]="4000000"
-    # ["kind-worker8"]="8000000"
-    # ["kind-worker9"]="16000000"
+    # ["kind-worker7"]="4000000m"
+    # ["kind-worker8"]="8000000m"
+    # ["kind-worker9"]="16000000m"
 )
 
 # Fake memory in MiB
 declare -A fakeMemory=(
     # Fog nodes
-    ["kind-control-plane"]="8192"
-    ["kind-worker"]="1024"
-    ["kind-worker2"]="1024"
-    ["kind-worker3"]="1024"
-    ["kind-worker4"]="4096"
-    ["kind-worker5"]="2048"
-    ["kind-worker6"]="2048"
+    ["kind-control-plane"]="8Gi"
+    ["kind-worker"]="1Gi"
+    ["kind-worker2"]="1Gi"
+    ["kind-worker3"]="1Gi"
+    ["kind-worker4"]="4Gi"
+    ["kind-worker5"]="2Gi"
+    ["kind-worker6"]="2Gi"
 
     # Cloud nodes (represent node types, the configured resources are actualAvailableResource * 1000)
-    # ["kind-worker7"]="2048000"
-    # ["kind-worker8"]="8192000"
-    # ["kind-worker9"]="16384000"
+    # ["kind-worker7"]="2000Gi"
+    # ["kind-worker8"]="8000Gi"
+    # ["kind-worker9"]="16000Gi"
 )
 
 # Costs of the nodes.
