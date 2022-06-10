aefceaff1564711fad87eaa543cf99662ac56a62
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-06-10 20:52:40 +0200

    Add test-app for scenario-02

---
+++ go/scheduler/hack/testbed/experiments/scenario-02/run-all-experiments.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# IMPORTANT: Configure the names of the scheduler pods here.
+# Use the following syntax: "<namespace>:<pod-name"
+# E.g., ["polaris-scheduler"]="polaris:polaris-scheduler-xyz"
+declare -A schedulerPods=(
+    ["greedy-first-fit-scheduler"]=""
+    ["round-robin-scheduler"]=""
+    ["timed-kube-scheduler"]=""
+    ["polaris-scheduler"]=""
+)
+
+# The number of iterations to execute.
+iterationsCount=5
+
+instanceCounts=(
+    "1"
+    # "8"
+)
+
+experimentFiles=(
+    "./test-app.template.yaml"
+)
+
+for scheduler in "${!schedulerPods[@]}"; do
+    if [ "${schedulerPods[$scheduler]}" == "" ]; then
+        echo "Please set the names of the scheduler pods in the schedulerPods array!"
+        exit 1
+    fi
+done
+
+mkdir -p "logs"
+
+for scheduler in "${!schedulerPods[@]}"; do
+    for instanceCount in "${instanceCounts[@]}"; do
+        for experimentFile in "${experimentFiles[@]}"; do
+            startInfo="$(date) Running experiment $experimentFile with $scheduler and $instanceCount instances"
+            echo "$startInfo"
+
+            ./run-single-experiment.sh "${experimentFile}" "${scheduler}" "${instanceCount}" "${iterationsCount}"
+
+            readarray -d : -t podId <<< "${schedulerPods[$scheduler]}"
+            podNamespace="${podId[0]}"
+            podName=$(echo "${podId[1]}" | tr -d "\n")
+            logFile="./logs/${scheduler}-${instanceCount}instances-$(date +%s).log"
+
+            if [ "$podName" != "" ]; then
+                echo "$startInfo" > "$logFile"
+                kubectl logs -n "$podNamespace" "$podName" >> "$logFile"
+            fi
+        done
+    done
+done
---
+++ go/scheduler/hack/testbed/experiments/scenario-02/run-single-experiment.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# set -x
+# set -o errexit
+
+function printUsage() {
+    echo "Usage:"
+    echo "./run-single-experiment.sh [deployment-YAML-template] [schedulerName] [instanceCount] [iterationsCount]"
+    echo "Example with 10 instances, 5 iterations, and polaris-scheduler:"
+    echo "./run-single-experiment.sh ./test-app.template.yaml polaris-scheduler 10 5"
+}
+
+if [ "$1" == "" ] || [ ! -f $1 ]; then
+    echo "Please provide the name of a deployment template YAML as the first argument."
+    printUsage
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    echo "Please provide the scheduler name as the second argument."
+    printUsage
+    exit 1
+fi
+
+if [ "$3" == "" ]; then
+    echo "Please provide the instance count as the third argument."
+    printUsage
+    exit 1
+fi
+
+if [ "$4" == "" ]; then
+    echo "Please provide the number of iterations as the fourth argument."
+    printUsage
+    exit 1
+fi
+
+deploymentConfigs=("$1")
+schedulerName="$2"
+instanceCount="$3"
+iterationsCount="$4"
+namespaceBase="traffic-safety-"
+shortSleepTime="20s"
+longSleepTime="1m"
+resultsDirSuffix="results"
+resultsDir=""
+totalPods=3
+deployedNamespaces=()
+separatorLines="\n---------------------------------------------------------------------------\n"
+
+# Waits until all pods are ready and appends the checking output to the log file.
+function waitForResult() {
+    local deployment=$1
+    local iteration=$2
+    local outFile="${resultsDir}/iteration${iteration}.json"
+
+    echo "$(date): kubectl apply complete."
+    sleepVerbosely $longSleepTime
+    echo -e "{\n  \"schedulerName\": \"${schedulerName}\",\n  \"deployment\": \"${deployment}\",\n  \"iteration\": ${iteration},\n  \"instanceCount\": ${instanceCount},\n  \"results\": [\n" > "$outFile"
+
+    for n in "${!deployedNamespaces[@]}"; do
+        local namespace=${deployedNamespaces[$n]}
+        if [ $n -gt 0 ]; then
+            echo -e ",\n" >> "$outFile"
+        fi
+        echo -e "{\n  \"namespace\": \"${namespace}\",\n    \"result\":\n" >> "$outFile"
+        local output=$(kubectl get pods -n $namespace -o json)
+        echo "$output" >> "$outFile"
+        echo "Checking namespace: ${namespace}"
+        echo -e "\n}" >> "$outFile"
+    done
+
+    echo -e "\n],  \"nodes\": " >> "$outFile"
+    kubectl get nodes -o json >> "$outFile"
+    echo -e "\n}" >> "$outFile"
+}
+
+function undeploy() {
+    local namespace=$1
+    echo "Deleting namespace $namespace"
+    kubectl delete namespace $namespace
+
+    local output=$(kubectl get pods -n $namespace -o wide)
+    while [ -n "$output" ]; do
+        sleepVerbosely $shortSleepTime
+        output=$(kubectl get pods -n $namespace -o wide)
+    done
+    echo "Deletion of namespace ${namespace} verified"
+}
+
+function sleepVerbosely() {
+    echo "Sleeping for $1"
+    sleep $1
+}
+
+function executeIteration() {
+    local deploymentTemplateFile=$1
+    local iteration=$2
+    deployedNamespaces=()
+    echo "Deploying iteration ${deploymentTemplateFile}-${iteration}"
+
+    local fullDeploymentYaml=""
+
+    for instance in $(seq 1 $instanceCount); do
+        local finalNamespace="${namespaceBase}${instance}"
+        local deploymentYaml=$(sed -e "s/{{ \.Namespace }}/${finalNamespace}/" ${deploymentTemplateFile})
+        deploymentYaml=$(echo "${deploymentYaml}" | sed -e "s/{{ \.SchedulerName }}/${schedulerName}/" -)
+        fullDeploymentYaml=$(echo -e "${fullDeploymentYaml}\n---\n${deploymentYaml}")
+        deployedNamespaces+=("${finalNamespace}")
+    done
+
+    echo "${fullDeploymentYaml}" | kubectl apply -f -
+    waitForResult $deployment $iteration
+
+    echo "Undeploying iteration ${iteration}"
+    for namespace in "${deployedNamespaces[@]}"; do
+        undeploy "$namespace"
+    done
+}
+
+
+for deployment in "${deploymentConfigs[@]}"; do
+    resultsDir="$(dirname "${deployment}")/${schedulerName}-${resultsDirSuffix}/$(basename ${deployment})-${instanceCount}instances"
+    mkdir -p "$resultsDir"
+    for i in $(seq 1 $iterationsCount ); do
+        echo "$deployment iteration: $i"
+        executeIteration "$deployment" $i
+        echo -e "\n"
+    done
+done
