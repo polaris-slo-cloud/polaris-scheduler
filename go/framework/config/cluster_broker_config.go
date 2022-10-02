ce16ef3b5f4e25e751ab22a7f9f9f0b7a0565fe3
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-10-02 19:24:05 +0200

    Add Dockerfiles

--- go/framework/config/cluster_broker_config.go
+++ go/framework/config/cluster_broker_config.go
@@ -18,13 +18,13 @@ type ClusterBrokerConfig struct {
 	// The update interval for the nodes cache in milliseconds.
 	//
 	// Default: 200
-	NodesCacheUpdateIntervalMs uint32
+	NodesCacheUpdateIntervalMs uint32 `json:"nodesCacheUpdateIntervalMs" yaml:"nodesCacheUpdateIntervalMs"`
 
 	// The size of the update queue in the nodes cache.
 	// The update queue caches watch events that arrive between the update intervals.
 	//
 	// Default: 1000
-	NodesCacheUpdateQueueSize uint32
+	NodesCacheUpdateQueueSize uint32 `json:"nodesCacheUpdateQueueSize" yaml:"nodesCacheUpdateQueueSize"`
 }
 
 // Sets the default values in the ClusterBrokerConfig for fields that are not set properly.
